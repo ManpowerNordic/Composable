14fa516b51f97b4f425b4656adaa5ef257b439fd
mlidbo <magnus.lidbom@gmail.com>
2015-11-02 18:25:03 +0100

    Add obsoletion comment.

--- Composable.CQRS/KeyValueStorage/IDocumentDbUpdater.cs
+++ Composable.CQRS/KeyValueStorage/IDocumentDbUpdater.cs
@@ -11,6 +11,7 @@ namespace Composable.KeyValueStorage
         /// <summary>Like TryGet but , if supported by implementing class, eagerly locks the instance in the database.</summary>
         bool TryGetForUpdate<TValue>(object key, out TValue value);
 
+        [Obsolete("Please do not use this horrible hack. Will be removed in the future.")]
         void Save<TValue>(object id, TValue value);
         void Delete<TEntity>(object id);
         void Save<TEntity>(TEntity entity) where TEntity : IHasPersistentIdentity<Guid>;
